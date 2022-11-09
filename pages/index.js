7cbe65f1b12e8a392bdf2725e0a8560e7e77c3ca
panosjapan7 <panosjapan@gmail.com>
2022-11-09 09:42:54 +0100

    Removes unused code and elements

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div style={{padding: "0 2rem"}}>
+    <div style={{ padding: "0 2rem" }}>
       <header>
         <h1>Nackademin Teknikfördjupning Projekt by Panagiotis Tsapanidis</h1>
         <h2>Next.js</h2>
-      </header>      
+      </header>
 
-      <main style={{width: "1600px", height: "1109px"}}>
-        <img src='/tiger-Siberian.jpg' alt="tiger" width="1600" height="1109"/>
-        <hr/>
-        <Image src='/tiger-Siberian.jpg'width="1600" height="1109" />
+      <main style={{ width: "1600px", height: "1109px" }}>
+        <Image src='/tiger-Siberian.jpg' width="1600" height="1109" />
       </main>
     </div>
   )
